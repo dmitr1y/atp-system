9e3d9be98b71011ed5d1899235733cc24b1d7396
Dmitriy Konstantinov <aser00707@ya.ru>
2017-02-12 02:06:52 +0300

    changed port

--- index.js
+++ index.js
@@ -14,59 +14,59 @@ var app         = express();
 
 // * Methods
 // Configuration
-app.set('port', (process.env.PORT || 7000));
+app.set('port', (process.env.PORT || 80));
 app.use(bodyParser.urlencoded({ extended: false })); // Client request type is "application/x-www-form-urlencoded"
 
 // Routing
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
-  console.log('info: Connection to server.')
+    res.sendFile(__dirname + '/index.html');
+    console.log('info: Connection to server.')
 });
 
 app.get('/css/:style', function(req, res) {
-  res.sendFile(__dirname + '/css/' + req.params.style);
+    res.sendFile(__dirname + '/css/' + req.params.style);
 });
 
 app.get('/js/:script', function(req, res) {
-  res.sendFile(__dirname + '/js/' + req.params.script);
+    res.sendFile(__dirname + '/js/' + req.params.script);
 });
 
 // POST request to root is to compute stuff taken from client
 app.post('/', function(req, res) {
-  var atp;
+    var atp;
 
-  if (req.body.alg != 'unf') { // Unification algorithm must take two terms. Otherwise it takes one formula.
-    atp = child.spawn('node', ['./js/atp/core.js', req.body.alg, req.body.fstInput]);
-  } else {
-    atp = child.spawn('node', ['./js/atp/core.js', req.body.alg, req.body.fstInput, req.body.sndInput]);
-  }
+    if (req.body.alg != 'unf') { // Unification algorithm must take two terms. Otherwise it takes one formula.
+        atp = child.spawn('node', ['./js/atp/core.js', req.body.alg, req.body.fstInput]);
+    } else {
+        atp = child.spawn('node', ['./js/atp/core.js', req.body.alg, req.body.fstInput, req.body.sndInput]);
+    }
 
-  if (debugMode) {
-    console.log('debug: Received algorithm: ' +
-      '\"' + req.body.alg + '\".');
-    console.log('debug: Received first input: ' +
-      '\"' + req.body.fstInput + '\".');
-    console.log('debug: Received second input: ' +
-      '\"' + req.body.sndInput + '\".');
-  }
+    if (debugMode) {
+        console.log('debug: Received algorithm: ' +
+            '\"' + req.body.alg + '\".');
+        console.log('debug: Received first input: ' +
+            '\"' + req.body.fstInput + '\".');
+        console.log('debug: Received second input: ' +
+            '\"' + req.body.sndInput + '\".');
+    }
 
-  atp.stdout.on('data', function(output) {
-    res.send(encodeURIComponent(output));
-    res.end();
+    atp.stdout.on('data', function(output) {
+        res.send(encodeURIComponent(output));
+        res.end();
 
-    console.log('info: Received correct message. Results sent to client.');
-  });
+        console.log('info: Received correct message. Results sent to client.');
+    });
 
-  // Output to stderr means something went wrong.
-  atp.stderr.on('data', function(output) {
-    res.send(encodeURIComponent(stderrStreamErrorMessage));
-    res.end();
+    // Output to stderr means something went wrong.
+    atp.stderr.on('data', function(output) {
+        res.send(encodeURIComponent(stderrStreamErrorMessage));
+        res.end();
 
-    console.log('error: Something bad happenned.');
-  });
+        console.log('error: Something bad happenned.');
+    });
 });
 
 // Initialization
 app.listen(app.get('port'), function () {
-  console.log('info: Server has started. Listening on port ' + app.get('port') + '.');
+    console.log('info: Server has started. Listening on port ' + app.get('port') + '.');
 });
